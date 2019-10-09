33974f666b62e22fd791c42ebec7e4e316dbd6a9
honest <honestxll@gmail.com>
2019-10-09 20:58:28 +0800

    显示数据（插值, ngFor, ngIf）

--- src/app/modules/post/post.component.ts
+++ src/app/modules/post/post.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
+  title = 'Posts';
+  entities = [
+    {
+      title: 'hello',
+      body:
+        'Unde maxime repellendus accusantium praesentium et quis velit, cumque optio minus?',
+    },
+    {
+      title: 'hola',
+      body:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam maiores suscipit, aspernatur ex eaque quas. Libero optio amet mollitia.',
+    },
+    { title: '你好' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

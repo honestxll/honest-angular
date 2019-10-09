fcbe6ff81ffe6c90278d08faa45706235c9978bc
honest <honestxll@gmail.com>
2019-10-09 21:39:39 +0800

    父组件监听在子组件在发生的事情

--- src/app/modules/post/post.component.ts
+++ src/app/modules/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from './model/post.model';
 
 @Component({
   selector: 'app-post',
@@ -9,26 +10,28 @@ export class PostComponent implements OnInit {
   title = 'Posts';
   entities = [
     {
+      id: 1,
       title: 'hello',
       body:
         'Unde maxime repellendus accusantium praesentium et quis velit, cumque optio minus?',
     },
     {
+      id: 2,
       title: 'hola',
       body:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam maiores suscipit, aspernatur ex eaque quas. Libero optio amet mollitia.',
     },
-    { title: '你好' },
+    { id: 3, title: '你好' },
   ];
 
   constructor() {}
 
   ngOnInit() {}
 
-  removeItem(item: number) {
+  removeItem(item: Post) {
     console.log('remove');
-    this.entities = this.entities.filter((entity, index) => {
-      return index !== item;
+    this.entities = this.entities.filter(entity => {
+      return entity.id !== item.id;
     });
   }
 }

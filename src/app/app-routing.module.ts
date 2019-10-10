e911d1ab784ef1fdd94ca56a2451242b3f3770ad
honest <1131328108@qq.com>
2019-10-10 13:12:12 +0800

    有名字的 outlets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { MessageBoxComponent } from './core/components/message-box/message-box.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'message', component: MessageBoxComponent, outlet: 'popup' },
   { path: '**', component: PageNotFoundComponent },
 ];
 

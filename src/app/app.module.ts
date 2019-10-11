19e45bc58ded0c497cd6b85c374431d3b001f0d9
honest <1131328108@qq.com>
2019-10-11 17:06:58 +0800

    ng 模块的导入导出和定义

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, UserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-
+import { UserComponent } from './user.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  declarations: [UserComponent],
+  imports: [CommonModule, UserRoutingModule],
+  exports: [UserComponent],
 })
-export class UserModule { }
+export class UserModule {}

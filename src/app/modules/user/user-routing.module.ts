14c75379fffecf8a1b93d468825040b0b40e14d6
honest <1131328108@qq.com>
2019-10-11 15:10:58 +0800

    我的内容列表

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
+import { MyListComponent } from './components/my-list/my-list.component';
 
-const routes: Routes = [{ path: 'register', component: RegisterComponent }];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'my-list',
+    component: MyListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
